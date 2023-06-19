62ae5972cb41bf9075774faaee7a1173317e812b
green-cabbage <hyeonseo_yun@hotmail.com>
2023-06-19 17:42:04 -0400

    updating to nanoaod and nanoaodsim plus higgs and triboson. Still have yet add electron data for 2018, and 2016 eras

--- rucio_container.sh
+++ rucio_container.sh
@@ -22,64 +22,103 @@ echo "adding rucio container"
 #elec_dataset+=(${TT2_dataset[@]})
 #
 
+# gather data
+data18=( $(dasgoclient --query="dataset = /SingleMuon/Run2018*-UL2018_MiniAODv2_NanoAODv9_GT36-v1/NANOAOD")) # add muon data
+
 dy18=( $(dasgoclient --query="dataset = /DYJetsToLL_*J_MLL_*_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18*NanoAOD*v9*/NANOAODSIM"))
-dy18+=( $(dasgoclient --query="dataset = /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM"))
+dy18+=( $(dasgoclient --query="dataset = /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"))
 # gather other_mc, starting with ttbar
-other_mc18=( $(dasgoclient --query="dataset = /TTToLL_MLL*_TuneCP5_13TeV*/RunIISummer20UL18MiniAODv2*/MINIAODSIM"))
-other_mc18+=( $(dasgoclient --query="dataset = /TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM"))
+other_mc18=( $(dasgoclient --query="dataset = /TTToLL_MLL*_TuneCP5_13TeV*/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+other_mc18+=( $(dasgoclient --query="dataset = /TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
 # WW
 other_mc18+=( $(dasgoclient --query="dataset = /WWTo2L2Nu_MLL_*_TuneCP5_13TeV*/RunIISummer20UL18NanoAODv9*/NANOAODSIM"))
 other_mc18+=( $(dasgoclient --query="dataset = /WWTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"))
 # rest of the other_mc
-other_mc18+=( $(dasgoclient --query="dataset = /ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM")) # Wantitop
-other_mc18+=( $(dasgoclient --query="dataset = /ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM")) # tW
-other_mc18+=( $(dasgoclient --query="dataset = /WZTo1L1Nu2Q_4f_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM")) # WZ1L1Nu2Q
-other_mc18+=( $(dasgoclient --query="dataset = /WZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM")) # WZ2L2Q
-other_mc18+=( $(dasgoclient --query="dataset = /WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM")) # WZ3LNu
-other_mc18+=( $(dasgoclient --query="dataset = /ZZTo2L2Nu_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM")) # ZZ2L2Nu
-other_mc18+=( $(dasgoclient --query="dataset = /ZZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM")) # ZZ2L2Q
-other_mc18+=( $(dasgoclient --query="dataset = /ZZTo4L_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL18MiniAODv2-106X_upgrade2018_realistic_v16_L1v1-v2/MINIAODSIM")) # ZZ4L
+other_mc18+=( $(dasgoclient --query="dataset = /ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM")) # Wantitop
+other_mc18+=( $(dasgoclient --query="dataset = /ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM")) # tW
+other_mc18+=( $(dasgoclient --query="dataset = /WZTo1L1Nu2Q_4f_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM")) # WZ1L1Nu2Q
+other_mc18+=( $(dasgoclient --query="dataset = /WZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM")) # WZ2L2Q
+other_mc18+=( $(dasgoclient --query="dataset = /WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM")) # WZ3LNu
+other_mc18+=( $(dasgoclient --query="dataset = /ZZTo2L2Nu_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM")) # ZZ2L2Nu
+other_mc18+=( $(dasgoclient --query="dataset = /ZZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM")) # ZZ2L2Q
+other_mc18+=( $(dasgoclient --query="dataset = /ZZTo4L_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM")) # ZZ4L
+# Higgs
+higgs_17=( $(dasgoclient --query="dataset = /TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ttHJetTobb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ttHJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /GluGluHToZZTo4L_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /VBF_HToZZTo4L_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ZH_HToBB_ZToLL_M-125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ggZH_HToBB_ZToLL_M-125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ttH_HToZZ_4LFilter_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /GluGluHToZZTo2L2Q_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ZH_HToZZ_4LFilter_M125_TuneCP5_13TeV_powheg2-minlo-HZJ_JHUGenV7011_pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM"))
 
-# gather data
-data18=( $(dasgoclient --query="dataset = /SingleMuon/Run2018A-UL2018_MiniAODv2_GT36-v1/MINIAOD"))
-data18+=( $(dasgoclient --query="dataset = /SingleMuon/Run2018B-UL2018_MiniAODv2_GT36-v1/MINIAOD"))
-data18+=( $(dasgoclient --query="dataset = /SingleMuon/Run2018C-UL2018_MiniAODv2_GT36-v1/MINIAOD"))
-data18+=( $(dasgoclient --query="dataset = /SingleMuon/Run2018D-UL2018_MiniAODv2_GT36-v1/MINIAOD"))
+# Triboson
+triboson_17=( $(dasgoclient --query="dataset = /WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+triboson_17+=( $(dasgoclient --query="dataset = /WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+triboson_17+=( $(dasgoclient --query="dataset = /WZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+triboson_17+=( $(dasgoclient --query="dataset = /ZZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM"))
+
+
+
+
 
 
 sample18=(${dy18[@]})
 sample18+=(${other_mc18[@]})
 sample18+=(${data18[@]})
 
-dy17=( $(dasgoclient --query="dataset = /DYJetsToLL_*J_MLL_*_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17*NanoAOD*v9*/NANOAODSIM"))
-dy17+=( $(dasgoclient --query="dataset = /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM"))
-# gather other_mc, starting with ttbar
-other_mc17=( $(dasgoclient --query="dataset = /TTToLL_MLL*_TuneCP5_13TeV*/RunIISummer20UL17MiniAODv2*/MINIAODSIM"))
-other_mc17+=( $(dasgoclient --query="dataset = /TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v1/MINIAODSIM"))
-# WW
-other_mc17+=( $(dasgoclient --query="dataset = /WWTo2L2Nu_MLL_*_TuneCP5_13TeV*/RunIISummer20UL17NanoAODv9*/NANOAODSIM"))
-other_mc17+=( $(dasgoclient --query="dataset = /WWTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM"))
-# rest of the other_mc
-other_mc17+=( $(dasgoclient --query="dataset = /ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v1/MINIAODSIM")) # Wantitop
-other_mc17+=( $(dasgoclient --query="dataset = /ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v1/MINIAODSIM")) # tW
-other_mc17+=( $(dasgoclient --query="dataset = /WZTo1L1Nu2Q_4f_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM")) # WZ1L1Nu2Q
-other_mc17+=( $(dasgoclient --query="dataset = /WZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM")) # WZ2L2Q
-other_mc17+=( $(dasgoclient --query="dataset = /WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM")) # WZ3LNu
-other_mc17+=( $(dasgoclient --query="dataset = /ZZTo2L2Nu_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v1/MINIAODSIM")) # ZZ2L2Nu
-other_mc17+=( $(dasgoclient --query="dataset = /ZZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM")) # ZZ2L2Q
-other_mc17+=( $(dasgoclient --query="dataset = /ZZTo4L_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM")) # ZZ4L
 
 # gather data
-data17=( $(dasgoclient --query="dataset = /SingleMuon/Run2017B-UL2017_MiniAODv2-v1/MINIAOD"))
-data17+=( $(dasgoclient --query="dataset = /SingleMuon/Run2017C-UL2017_MiniAODv2-v1/MINIAOD"))
-data17+=( $(dasgoclient --query="dataset = /SingleMuon/Run2017D-UL2017_MiniAODv2-v1/MINIAOD"))
-data17+=( $(dasgoclient --query="dataset = /SingleMuon/Run2017E-UL2017_MiniAODv2-v1/MINIAOD"))
-data17+=( $(dasgoclient --query="dataset = /SingleMuon/Run2017F-UL2017_MiniAODv2-v1/MINIAOD"))
+data17=( $(dasgoclient --query="dataset = /SingleMuon/Run2017*-UL2017_MiniAODv2_NanoAODv9_GT36-v1/NANOAOD")) # add muon data
+data17+=( $(dasgoclient --query="dataset = /DoubleEG/Run2017*-UL2017_MiniAODv2_NanoAODv9-v1/NANOAOD")) # add electron data
+
+dy17=( $(dasgoclient --query="dataset = /DYJetsToLL_*J_MLL_*_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17*NanoAOD*v9*/NANOAODSIM"))
+dy17+=( $(dasgoclient --query="dataset = /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM"))
+# gather other_mc, starting with ttbar
+other_mc17=( $(dasgoclient --query="dataset = /TTToLL_MLL*_TuneCP5_13TeV*/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+other_mc17+=( $(dasgoclient --query="dataset = /TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+# WW
+other_mc17+=( $(dasgoclient --query="dataset = /WWTo2L2Nu_MLL_*_TuneCP5_13TeV*/RunIISummer20UL17NanoAODv9*/NANOAODSIM"))
+other_mc17+=( $(dasgoclient --query="dataset = /WWTo2L2Nu_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM"))
+# rest of the other_mc
+other_mc17+=( $(dasgoclient --query="dataset = /ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM")) # Wantitop
+other_mc17+=( $(dasgoclient --query="dataset = /ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM")) # tW
+other_mc17+=( $(dasgoclient --query="dataset = /WZTo1L1Nu2Q_4f_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM")) # WZ1L1Nu2Q
+other_mc17+=( $(dasgoclient --query="dataset = /WZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM")) # WZ2L2Q
+other_mc17+=( $(dasgoclient --query="dataset = /WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM")) # WZ3LNu
+other_mc17+=( $(dasgoclient --query="dataset = /ZZTo2L2Nu_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM")) # ZZ2L2Nu
+other_mc17+=( $(dasgoclient --query="dataset = /ZZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM")) # ZZ2L2Q
+other_mc17+=( $(dasgoclient --query="dataset = /ZZTo4L_TuneCP5_13TeV_powheg_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM")) # ZZ4L
+# Higgs
+higgs_17=( $(dasgoclient --query="dataset = /TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ttHJetTobb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ttHJetToNonbb_M125_TuneCP5_13TeV_amcatnloFXFX_madspin_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /GluGluHToZZTo4L_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /VBF_HToZZTo4L_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ZH_HToBB_ZToLL_M-125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ggZH_HToBB_ZToLL_M-125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ttH_HToZZ_4LFilter_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /GluGluHToZZTo2L2Q_M125_TuneCP5_13TeV_powheg2_JHUGenV7011_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM"))
+higgs_17+=( $(dasgoclient --query="dataset = /ZH_HToZZ_4LFilter_M125_TuneCP5_13TeV_powheg2-minlo-HZJ_JHUGenV7011_pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v2/NANOAODSIM"))
+
+# Triboson
+triboson_17=( $(dasgoclient --query="dataset = /WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+triboson_17+=( $(dasgoclient --query="dataset = /WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+triboson_17+=( $(dasgoclient --query="dataset = /WZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
+triboson_17+=( $(dasgoclient --query="dataset = /ZZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIISummer20UL17NanoAODv9-106X_mc2017_realistic_v9-v1/NANOAODSIM"))
 
 
-sample17=(${dy17[@]})
-sample17+=(${other_mc17[@]})
-sample17+=(${data17[@]})
+
+sample17=(${data17[@]})
+# sample17+=(${dy17[@]})
+# sample17+=(${other_mc17[@]})
+sample17+=(${higgs_17[@]})
+sample17+=(${triboson_17[@]})
+
 # dy3=( $(dasgoclient --query="dataset = /DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16NanoAODv9-106X_mcRun2_asymptotic_v17-v1/NANOAODSIM"))
 
 # dy4=( $(dasgoclient --query="dataset = /DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL16NanoAODAPVv9-106X_mcRun2_asymptotic_preVFP_v11-v1/NANOAODSIM"))
